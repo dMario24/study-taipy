730c53e63c8632a4ba433c3c019cde8596307654
dmario24 <data.mario24@gmail.com>
2023-09-11 13:07:39 +0900

    step_03

--- src/study_taipy/firstweb.py
+++ src/study_taipy/firstweb.py
@@ -1,4 +1,4 @@
-from taipy.gui import Gui
+from taipy.gui import Gui, notify
 
 debug_mode = True
 
@@ -7,9 +7,23 @@ text = "Original text"
 page = """
 # Gettting started with Taipy GUI
 
-My text: <|<{text}>|>
+My text: <|{text}|>
 
 <|{text}|input|>
+
+<|Run local|button|on_action=on_button_action|>
 """
+
+
+def on_button_action(state):
+    notify(state, 'info', f'The text is: {state.text}')
+    state.text = "Button Pressed"
+
+
+def on_change(state, var_name, var_value):
+    if var_name == "text" and var_value == "Reset":
+        state.text = ""
+        return
+
 
 Gui(page=page).run(port=7942, use_reloader=debug_mode)
